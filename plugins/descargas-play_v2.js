dbdc112d8eb50e403946e000bb71a006ee3c7bc1
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-05-05 03:31:49 +0300

    Update descargas-play_v2.js

--- plugins/descargas-play_v2.js
+++ plugins/descargas-play_v2.js
@@ -1,211 +1,104 @@
-/*
-
-- Agradecimiento a la comunidad de "WSApp • Developers"
- * https://chat.whatsapp.com/FaQunmlp9BmDRk6lEEc9FJ
-- Agradecimiento especial a Carlos (PT) por los codigos de interactiveMessage (botones)
-- Agradecimiento a Darlyn1234 por la estructura de uso en este codigo y quoted
- * https://github.com/darlyn1234
-- Adaptacion de imagen en tipo lista, codigo y funcionamiento por BrunoSobrino
- * https://github.com/BrunoSobrino
-
-*/
 import fetch from 'node-fetch';
-import { prepareWAMessageMedia, generateWAMessageFromContent, getDevice } from '@whiskeysockets/baileys';
+import yts from 'yt-search';
+import ytdl from 'ytdl-core';
+import axios from 'axios';
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
 
-let data;
-let buff;
-let mimeType;
-let fileName;
-let apiUrl;
-let enviando = false;
-let device;
-const handler = async (m, { command, usedPrefix, conn, text }) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.descargas_play_v2
-  device = await getDevice(m.key.id);
-  
-  if (!text) throw `${tradutor.texto1[0]} _${usedPrefix + command} ${tradutor.texto1[1]} _${usedPrefix + command} https://youtu.be/JLWRZ8eWyZo?si=EmeS9fJvS_OkDk7p_`;
-  if (command === 'playyt' && device == 'desktop' || command === 'playyt' && device == 'web') throw `*[❗] Los mensajes de botones aun no estan disponibles en WhatsApp web, acceda a su celular para poder ver y usar los mensajes con botones.*`
-  if (enviando) return;
-      enviando = true
+const handler = async (m, { conn, command, args, text, usedPrefix }) => {
+  const datas = global;
+  const idioma = datas.db.data.users[m.sender].language;
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`));
+  const tradutor = _translate.plugins.downloader_playdoc;
+
+  if (!text) throw `${tradutor.texto1} *\n*${usedPrefix + command} Good Feeling - Flo Rida* `;
   try {
-      
-    const apiUrls = [
-      `https://api.cafirexos.com/api/ytplay?text=${text}`,
-      `https://api-brunosobrino.onrender.com/api/ytplay?text=${text}`,
-      `https://api-for-canvas-brunosobrino.koyeb.app/api/ytplay?text=${text}`,  
-    ];
-
-    for (const url of apiUrls) {
+    const yt_play = await search(args.join(' '));
+    let additionalText = '';
+    let buttonText = '';
+    if (command === 'play3' || command == 'playdoc') {
+      additionalText = 'audio 🔊';
+      buttonText = '🎵 Audio';
+    } else if (command === 'play4' || command == 'playdoc2') {
+      additionalText = 'video 🎥';
+      buttonText = '🎥 Video';
+    }
+    const texto1 = `${tradutor.texto2[0]}\n
+    ${tradutor.texto2[1]} ${yt_play[0].title}
+    ${tradutor.texto2[2]} ${yt_play[0].ago}
+    ${tradutor.texto2[3]} ${secondString(yt_play[0].duration.seconds)}
+    ${tradutor.texto2[4]} ${`${MilesNumber(yt_play[0].views)}`}
+    ${tradutor.texto2[5]} ${yt_play[0].author.name}
+    ${tradutor.texto2[6]} ${yt_play[0].author.url}
+    ${tradutor.texto2[7]} ${yt_play[0].videoId}
+    ${tradutor.texto2[8]} ${yt_play[0].type}
+    ${tradutor.texto2[9]} ${yt_play[0].url}\n
+    ${tradutor.texto2[10]} ${additionalText}, ${tradutor.texto2[11]}`.trim();
+    conn.sendMessage(m.chat, { image: { url: yt_play[0].thumbnail }, caption: texto1, buttons: [{ buttonId: `${command === 'play3' || command == 'playdoc' ? '#ytmp3doc' : '#ytmp4doc'} ${yt_play[0].url}`, buttonText: { displayText: buttonText }, type: 1 }] }, { quoted: m });
+    if (command == 'play3' || command == 'playdoc') {
       try {
-        const res = await fetch(url);
-        data = await res.json();
-        if (data.resultado && data.resultado.url) {
-          break;
-        }
-      } catch {}
-    }
-
-    if (!data.resultado || !data.resultado.url) {
-      enviando = false;
-      throw `${tradutor.texto2}`;
-    }      
-      
-    if (command === 'playyt') {
-        
-        const dataMessagee = `${tradutor.texto4[0]} ${data.resultado.title}\n${tradutor.texto4[1]} ${data.resultado.publicDate}\n${tradutor.texto4[2]} ${data.resultado.channel}\n${tradutor.texto4[3]} ${data.resultado.url}`.trim();
-        var messa = await prepareWAMessageMedia({ image: {url: data.resultado.image}}, { upload: conn.waUploadToServer })
-        let msg = generateWAMessageFromContent(m.chat, {
-            viewOnceMessage: {
-                message: {
-                    interactiveMessage: {
-                        body: { text: dataMessagee },
-                        footer: { text: `${global.wm}`.trim() },
-                        header: {
-                            hasMediaAttachment: true,
-                            imageMessage: messa.imageMessage,
-                        },
-                        nativeFlowMessage: {
-                            buttons: [
-                                {
-                                    name: 'quick_reply',
-                                    buttonParamsJson: JSON.stringify({
-                                        display_text: 'AUDIO',
-                                        id: `${usedPrefix}play.1 ${data.resultado.url} SN@`
-                                    })
-                                },
-                                {
-                                    name: 'quick_reply',
-                                    buttonParamsJson: JSON.stringify({
-                                        display_text: 'VIDEO',
-                                        id: `${usedPrefix}play.2 ${data.resultado.url} SN@`
-                                    })
-                                },   
-                                /*{
-                                    // URL Redirect 
-                                    name: 'cta_url',
-                                    buttonParamsJson: JSON.stringify({
-                                        display_text: 'IR A YOUTUBE',
-                                        url: `${data.resultado.url}`,
-                                        merchant_url: `${data.resultado.url}`,
-                                        id: `${data.resultado.url}`
-                                    })
-                                },*/                                
-                            ],
-                            messageParamsJson: "",
-                        },
-                    },
-                },
-            }
-        }, { userJid: conn.user.jid, quoted: m})
-      conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id});
-  enviando = false;    
-return;
-  }    
-      
-      try {      
-        if (command === 'play.1') { 
-              apiUrl = `https://api.cafirexos.com/api/v1/ytmp3?url=${data.resultado.url}`;
-              mimeType = 'audio/mpeg';
-              fileName = 'error.mp3';
-              buff = await conn.getFile(apiUrl);          
-            } else if (command === 'play.2') { 
-              apiUrl = `https://api.cafirexos.com/api/v1/ytmp4?url=${data.resultado.url}`;
-              mimeType = 'video/mp4';
-              fileName = 'error.mp4';
-              buff = await conn.getFile(apiUrl);        
-        }
-      } catch {        
+        const q = '128kbps';
+        const v = yt_play[0].url;
+        const yt = await youtubedl(v).catch(async (_) => await youtubedlv2(v));
+        const dl_url = await yt.audio[q].download();
+        const ttl = await yt.title;
+        const size = await yt.audio[q].fileSizeH;
+        await conn.sendMessage(m.chat, { document: { url: dl_url }, mimetype: 'audio/mpeg', fileName: `${ttl}.mp3` }, { quoted: m });
+      } catch {
+        try {
+          const lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytaudio2?apikey=${lolkeysapi}&url=${yt_play[0].url}`);
+          const lolh = await lolhuman.json();
+          const n = lolh.result.title || 'error';
+          await conn.sendMessage(m.chat, { document: { url: lolh.result.link }, fileName: `${n}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m });
+        } catch {
           try {
-            if (command === 'play.1') {
-              apiUrl = `https://api.cafirexos.com/api/v2/ytmp3?url=${data.resultado.url}`;
-              mimeType = 'audio/mpeg';
-              fileName = 'error.mp3';
-              buff = await conn.getFile(apiUrl);              
-            } else if (command === 'play.2') { 
-              apiUrl = `https://api.cafirexos.com/api/v2/ytmp4?url=${data.resultado.url}`;
-              mimeType = 'video/mp4';
-              fileName = 'error.mp4';
-              buff = await conn.getFile(apiUrl);              
-            }
+            const searchh = await yts(yt_play[0].url);
+            const __res = searchh.all.map((v) => v).filter((v) => v.type == 'video');
+            const infoo = await ytdl.getInfo('https://youtu.be/' + __res[0].videoId);
+            const ress = await ytdl.chooseFormat(infoo.formats, { filter: 'audioonly' });
+            conn.sendMessage(m.chat, { audio: { url: ress.url }, fileName: __res[0].title + '.mp3', mimetype: 'audio/mp4' }, { quoted: m });
           } catch {
-              try {
-         if (command === 'play.1') { 
-              apiUrl = `https://api-brunosobrino.onrender.com/api/v1/ytmp3?url=${data.resultado.url}`;
-              mimeType = 'audio/mpeg';
-              fileName = 'error.mp3';
-              buff = await conn.getFile(apiUrl);          
-            } else if (command === 'play.2') { 
-              apiUrl = `https://api-brunosobrino.onrender.com/api/v1/ytmp4?url=${data.resultado.url}`;
-              mimeType = 'video/mp4';
-              fileName = 'error.mp4';
-              buff = await conn.getFile(apiUrl);        
-        }
-      } catch { 
-              try {
-         if (command === 'play.1') { 
-              apiUrl = `https://api-brunosobrino.onrender.com/api/v2/ytmp3?url=${data.resultado.url}`;
-              mimeType = 'audio/mpeg';
-              fileName = 'error.mp3';
-              buff = await conn.getFile(apiUrl);          
-            } else if (command === 'play.2') { 
-              apiUrl = `https://api-brunosobrino.onrender.com/api/v2/ytmp4?url=${data.resultado.url}`;
-              mimeType = 'video/mp4';
-              fileName = 'error.mp4';
-              buff = await conn.getFile(apiUrl);        
-        }
-      } catch {   
-           try {
-         if (command === 'play.1') { 
-              apiUrl = `https://api-for-canvas-brunosobrino.koyeb.app/api/v1/ytmp3?url=${data.resultado.url}`;
-              mimeType = 'audio/mpeg';
-              fileName = 'error.mp3';
-              buff = await conn.getFile(apiUrl);          
-            } else if (command === 'play.2') { 
-              apiUrl = `https://api-for-canvas-brunosobrino.koyeb.app/api/v1/ytmp4?url=${data.resultado.url}`;
-              mimeType = 'video/mp4';
-              fileName = 'error.mp4';
-              buff = await conn.getFile(apiUrl);        
-        }
-      } catch {                  
-          try {
-         if (command === 'play.1') { 
-              apiUrl = `https://api-for-canvas-brunosobrino.koyeb.app/api/v2/ytmp3?url=${data.resultado.url}`;
-              mimeType = 'audio/mpeg';
-              fileName = 'error.mp3';
-              buff = await conn.getFile(apiUrl);          
-            } else if (command === 'play.2') { 
-              apiUrl = `https://api-for-canvas-brunosobrino.koyeb.app/api/v2/ytmp4?url=${data.resultado.url}`;
-              mimeType = 'video/mp4';
-              fileName = 'error.mp4';
-              buff = await conn.getFile(apiUrl);        
-        }
-      } catch {                           
-            enviando = false;
-            throw `${tradutor.texto3}`;
+            await conn.reply(m.chat, tradutor.texto3, m);
           }
-       }
-    }}
- }}
-
-    const dataMessage = `${tradutor.texto4[0]} ${data.resultado.title}\n${tradutor.texto4[1]} ${data.resultado.publicDate}\n${tradutor.texto4[2]} ${data.resultado.channel}\n${tradutor.texto4[3]} ${data.resultado.url}`;
-    if (!text.includes('SN@')) await conn.sendMessage(m.chat, { text: dataMessage }, { quoted: m });
-
-    if (buff) {
-      await conn.sendMessage(m.chat, {[mimeType.startsWith('audio') ? 'audio' : 'video']: buff.data, mimetype: mimeType, fileName: fileName}, {quoted: m});
-      enviando = false;
-    } else {
-      enviando = false;
-      throw `${tradutor.texto5}`;
+        }
+      }
     }
-  } catch (error) {
-    console.log(error)  
-    enviando = false;
-    throw tradutor.texto6;
+    if (command == 'play4' || command == 'playdoc2') {
+      try {
+        const qu = '360';
+        const q = qu + 'p';
+        const v = yt_play[0].url;
+        const yt = await youtubedl(v).catch(async (_) => await youtubedlv2(v));
+        const dl_url = await yt.video[q].download();
+        const ttl = await yt.title;
+        const size = await yt.video[q].fileSizeH;
+        await await conn.sendMessage(m.chat, { document: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `${tradutor.texto4[0]} ${ttl}\n${tradutor.texto4[1]} ${size}`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m });
+      } catch {
+        try {
+          const mediaa = await ytMp4(yt_play[0].url);
+          await await conn.sendMessage(m.chat, { document: { url: dl_url }, caption: cap, mimetype: 'video/mp4', fileName: ttl + `.mp4` }, { quoted: m });
+        } catch {
+          try {
+            const lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=${lolkeysapi}&url=${yt_play[0].url}`);
+            const lolh = await lolhuman.json();
+            const n = lolh.result.title || 'error';
+            const n2 = lolh.result.link;
+            const n3 = lolh.result.size;
+            const n4 = lolh.result.thumbnail;
+            await conn.sendMessage(m.chat, { document: { url: n2 }, fileName: `${n}.mp4`, mimetype: 'video/mp4', caption: `${tradutor.texto4[0]} ${n}\n${tradutor.texto4[1]} ${n3}`, thumbnail: await fetch(n4) }, { quoted: m });
+          } catch {
+            await conn.reply(m.chat, `${tradutor.texto5}`, m);
+          }
+        }
+      }
+    }
+  } catch {
+    throw `${tradutor.texto6}}`;
   }
 };
-handler.command = /^(play.1|play.2|playyt)$/i;
+handler.help = ['play3', 'play4'].map((v) => v + ' <busqueda>');
+handler.tags = ['downloader'];
+handler.command = /^(playdoc|playdoc2|play3|play4)$/i;
 export default handler;
 
+async function search(query, options = {}) {
+  const search = await yts.search({ query, hl: 'es',
