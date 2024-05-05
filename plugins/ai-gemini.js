afa7b757380f7e4580c7b8a42ed50123658521c3
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-05-05 20:15:25 +0300

    Update and rename gemini.js to ai-gemini.js

---
+++ plugins/ai-gemini.js
@@ -0,0 +1,40 @@
+import fetch from  node-fetch ;
+
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+  try {
+    
+    if (!text) {
+      return conn.reply(m.chat, `*مرحبا كيف يمكنني مساعدتك اليوم*`, m);
+    }
+      
+    await m.reply(wait)
+
+    const response = await fetch(`https://aemt.me/gemini?text=${encodeURIComponent(text)}`);
+    if (!response.ok) {
+      return conn.reply(m.chat,  أسف هل تقدر على صياغة السؤال مجددا , m);
+    }
+
+    const data = await response.json();
+    const result = data.result;
+
+    if (!result) {
+      return conn.reply(m.chat,  أسف حدث خطأ في تنفيد طلبك حاول لاحقا . , m);
+    }
+
+    conn.reply(m.chat, result, m);
+  } catch (error) {
+    
+    throw eror
+  }
+};
+
+handler.help = [ gemini ];
+handler.tags = [ ai ];
+handler.limit = false;
+handler.register = false;
+
+handler.command = /^(بوت)$/i;
+
+export default handler;
+
+// Ni plugens
