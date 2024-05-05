7b4454faf7c7128df8e9e49d881e613429ad79d8
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-05-05 21:18:09 +0300

    Update descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -64,7 +64,7 @@ const handler = async (m, {conn, args, command, usedPrefix}) => {
     }
   }
 };
-handler.command = /^(facebook|fb|facebookdl|fbdl|facebook2|fb2|facebookdl2|fbdl2|facebook3|fb3|facebookdl3|fbdl3|facebook4|fb4|facebookdl4|fbdl4|facebook5|fb5|facebookdl5|fbdl5)$/i;
+handler.command = /^(facebook|fb|فيس|fbdl|facebook2|fb2|فيسبوك|fbdl2|facebook3|fb3|facebookdl3|fbdl3|facebook4|fb4|facebookdl4|fbdl4|facebook5|fb5|facebookdl5|fbdl5)$/i;
 export default handler;
 
 async function igeh(url_media) {
