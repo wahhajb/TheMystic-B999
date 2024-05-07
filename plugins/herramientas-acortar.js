bf60e6b44e64245e25926c7847e958c4d494eaa3
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-05-07 21:26:42 +0300

    Update herramientas-acortar.js

--- plugins/herramientas-acortar.js
+++ plugins/herramientas-acortar.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 const handler = async (m, {conn, args, text}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
   const tradutor = _translate.plugins.herramientas_acortar
 
   if (!text) throw tradutor.texto1;
@@ -14,6 +14,6 @@ const handler = async (m, {conn, args, text}) => {
 };
 handler.help = ['tinyurl', 'acortar'].map((v) => v + ' <link>');
 handler.tags = ['tools'];
-handler.command = /^(tinyurl|short|acortar|corto)$/i;
+handler.command = /^(tinyurl|short|قص|تقصير)$/i;
 handler.fail = null;
 export default handler;
