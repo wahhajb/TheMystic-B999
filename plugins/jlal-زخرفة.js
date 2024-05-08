05d8fbb8f2de5f33c53d4c9bf049b33ee09254bc
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-05-08 16:25:59 +0300

    Create jlal-زخرفة.js

---
+++ plugins/jlal-زخرفة.js
@@ -0,0 +1,26 @@
+let handler = async(m, { conn, text , command }) => {
+if (command == 'focus') {
+if (!text) throw '*مثال* :\n*.focus* ♥♥';
+let awikwok = `👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿
+👉🏿👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👈🏿
+👉🏿👉🏾👇🏽👇🏽👇🏽👇🏽👇🏽👇🏽👇🏽👈🏾👈🏿
+👉🏿👉🏾👉🏽👇🏼👇🏼👇🏼👇🏼👇🏼👈🏽👈🏾👈🏿
+👉🏿👉🏾👉🏽👉🏼👇🏻👇🏻👇🏻👈🏼👈🏽👈🏾👈🏿
+👉🏿👉🏾👉🏽👉🏼👉🏻${text}👈🏻👈🏼👈🏽👈🏾👈🏿
+👉🏿👉🏾👉🏽👉🏼👆🏻👆🏻👆🏻👈🏼👈🏽👈🏾👈🏿
+👉🏿👉🏾👉🏽👆🏼👆🏼👆🏼👆🏼👆🏼👈🏽👈🏾👈🏿
+👉🏿👉🏾👆🏽👆🏽👆🏽👆🏽👆🏽👆🏽👈🏽👈🏾👈🏿
+👉🏿👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👈🏿
+👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿`
+m.reply(awikwok, null, m.mentionedJid ? {
+  mentions: m.mentionedJid
+} : {})
+}
+}
+handler.command = handler.help = ['focus']
+handler.tags = ['زخرفه']
+export default handler
+
+
+
+┃𝒔𝒕𝒐𝒏𝒆
