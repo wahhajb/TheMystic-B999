5a4afe4b094bd49c59567ba6575fc5ff928547d2
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-05-14 18:13:49 +0300

    Update and rename _الرد.js to -بوت-منفردردالي.js

--- plugins/-بوت-منفردردالي.js
+++ plugins/-بوت-منفردردالي.js
@@ -6,11 +6,11 @@
 
     // تقسيم النص إلى كلمات والتحقق من وجود الكلمة "بوت" ككلمة منفردة
     let words = m.text.split(" ");
-    if (words.includes("بوت.")) {
+    if (words.includes("بوت")) {
         conn.reply(m.chat, teks, m, { mentions: { mentionedJid: [m.sender] }});
     }
 }
-handler.customPrefix = /(بوت.)$/i;
+handler.customPrefix = /(بوت)$/i;
 handler.command = new RegExp;
 
 export default handler;
