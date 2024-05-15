001957e116ef6b32dbd20ef90943daaee252da01
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-05-15 20:47:54 +0300

    Update dl-songs.js

--- plugins/dl-songs.js
+++ plugins/dl-songs.js
@@ -1,10 +1,11 @@
-import fetch from 'node-fetch';
+ import fetch from 'node-fetch';
 import yts from 'yt-search';
 import ytdl from 'ytdl-core';
 import axios from 'axios';
-import {youtubedl, youtubedlv2} from '@bochilteam/scraper';
-const handler = async (m, {conn, command, args, text, usedPrefix}) => {
-  if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙵𝙰𝙻𝚃𝙰𝙽𝚃𝙴, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴/𝚃𝙸𝚃𝚄𝙻𝙾 𝙳𝙴 𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} Good Feeling - Flo Rida* `;
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
+
+const handler = async (m, { conn, command, args, text, usedPrefix }) => {
+  if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙵𝙰𝙻𝚃𝙰𝙽𝚃𝙴, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴/𝚃𝙸𝚃𝚄𝙻𝙾 𝙳𝙴 𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} Good Feeling - Flo Rida*`;
   try {
     const yt_play = await search(args.join(' '));
     let additionalText = '';
@@ -14,7 +15,8 @@ const handler = async (m, {conn, command, args, text, usedPrefix}) => {
       additionalText = 'video 🎥';
     }
     const message = "جاري تحميل طلبك...";
-conn.sendMessage(m.chat, message, { quoted: m });
+    conn.sendMessage(m.chat, message, { quoted: m });
+
     if (command == 'play3' || command == 'playdoc') {
       try {
         const q = '128kbps';
@@ -22,27 +24,27 @@ conn.sendMessage(m.chat, message, { quoted: m });
         const yt = await youtubedl(v).catch(async (_) => await youtubedlv2(v));
         const dl_url = await yt.audio[q].download();
         const ttl = await yt.title;
-        const size = await yt.audio[q].fileSizeH;
-        await conn.sendMessage(m.chat, {document: {url: dl_url}, mimetype: 'audio/mpeg', fileName: `${ttl}.mp3`}, {quoted: m});
+        await conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: 'audio/mpeg', fileName: `${ttl}.mp3` }, { quoted: m });
       } catch {
         try {
           const lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytaudio2?apikey=${lolkeysapi}&url=${yt_play[0].url}`);
           const lolh = await lolhuman.json();
           const n = lolh.result.title || 'error';
-          await conn.sendMessage(m.chat, {document: {url: lolh.result.link}, fileName: `${n}.mp3`, mimetype: 'audio/mpeg'}, {quoted: m});
+          await conn.sendMessage(m.chat, { audio: { url: lolh.result.link }, fileName: `${n}.mp3`, mimetype: 'audio/mpeg' }, { quoted: m });
         } catch {
           try {
             const searchh = await yts(yt_play[0].url);
             const __res = searchh.all.map((v) => v).filter((v) => v.type == 'video');
             const infoo = await ytdl.getInfo('https://youtu.be/' + __res[0].videoId);
-            const ress = await ytdl.chooseFormat(infoo.formats, {filter: 'audioonly'});
-            conn.sendMessage(m.chat, {audio: {url: ress.url}, fileName: __res[0].title + '.mp3', mimetype: 'audio/mp4'}, {quoted: m});
+            const ress = await ytdl.chooseFormat(infoo.formats, { filter: 'audioonly' });
+            conn.sendMessage(m.chat, { audio: { url: ress.url }, fileName: __res[0].title + '.mp3', mimetype: 'audio/mp4' }, { quoted: m });
           } catch {
             await conn.reply(m.chat, '*[❗] 𝙴𝚁𝚁𝙾𝚁 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝙴𝙻 𝙰𝚄𝙳𝙸𝙾*', m);
           }
         }
       }
     }
+
     if (command == 'play4' || command == 'playdoc2') {
       try {
         const qu = '360';
@@ -51,21 +53,18 @@ conn.sendMessage(m.chat, message, { quoted: m });
         const yt = await youtubedl(v).catch(async (_) => await youtubedlv2(v));
         const dl_url = await yt.video[q].download();
         const ttl = await yt.title;
-        const size = await yt.video[q].fileSizeH;
-        await await conn.sendMessage(m.chat, {document: {url: dl_url}, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `▢ 𝚃𝙸𝚃𝚄𝙻𝙾: ${ttl}\n▢ 𝙿𝙴𝚂𝙾 𝙳𝙴𝙻 𝚅𝙸𝙳𝙴𝙾: ${size}`, thumbnail: await fetch(yt.thumbnail)}, {quoted: m});
+        await conn.sendMessage(m.chat, { video: { url: dl_url }, mimetype: 'video/mp4', fileName: `${ttl}.mp4`, caption: `▢ 𝚃𝙸𝚃𝚄𝙻𝙾: ${ttl}` }, { quoted: m });
       } catch {
         try {
           const mediaa = await ytMp4(yt_play[0].url);
-          await await conn.sendMessage(m.chat, {document: {url: dl_url}, caption: cap, mimetype: 'video/mp4', fileName: ttl + `.mp4`}, {quoted: m});
+          await conn.sendMessage(m.chat, { video: { url: mediaa.result }, caption: mediaa.title, mimetype: 'video/mp4', fileName: `${mediaa.title}.mp4` }, { quoted: m });
         } catch {
           try {
             const lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=${lolkeysapi}&url=${yt_play[0].url}`);
             const lolh = await lolhuman.json();
             const n = lolh.result.title || 'error';
             const n2 = lolh.result.link;
-            const n3 = lolh.result.size;
-            const n4 = lolh.result.thumbnail;
-            await conn.sendMessage(m.chat, {document: {url: n2}, fileName: `${n}.mp4`, mimetype: 'video/mp4', caption: `▢ 𝚃𝙸𝚃𝚄𝙻𝙾: ${n}\n▢ 𝙿𝙴𝚂𝙾 𝙳𝙴𝙻 𝚅𝙸𝙳𝙴𝙾: ${n3}`, thumbnail: await fetch(n4)}, {quoted: m});
+            await conn.sendMessage(m.chat, { video: { url: n2 }, fileName: `${n}.mp4`, mimetype: 'video/mp4', caption: `▢ 𝚃𝙸𝚃𝚄𝙻𝙾: ${n}` }, { quoted: m });
           } catch {
             await conn.reply(m.chat, '*[❗] 𝙴𝚁𝚁𝙾𝚁 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾*', m);
           }
@@ -76,13 +75,14 @@ conn.sendMessage(m.chat, message, { quoted: m });
     throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*';
   }
 };
+
 handler.help = ['play3', 'play4'].map((v) => v + ' < busqueda >');
 handler.tags = ['downloader'];
 handler.command = /^(playdoc|playdoc2|play3|play4)$/i;
 export default handler;
 
 async function search(query, options = {}) {
-  const search = await yts.search({query, hl: 'es', gl: 'ES', ...options});
+  const search = await yts.search({ query, hl: 'es', gl: 'ES', ...options });
   return search.videos;
 }
 
@@ -104,7 +104,7 @@ function secondString(seconds) {
   const hDisplay = h > 0 ? h + (h == 1 ? ' hora, ' : ' horas, ') : '';
   const mDisplay = m > 0 ? m + (m == 1 ? ' minuto, ' : ' minutos, ') : '';
   const sDisplay = s > 0 ? s + (s == 1 ? ' segundo' : ' segundos') : '';
-  return dDisplay + hDisplay + mDisplay + sDisplay;
+  return dreturn dDisplay + hDisplay + mDisplay + sDisplay;
 }
 
 function bytesToSize(bytes) {
@@ -123,10 +123,10 @@ async function ytMp3(url) {
       const result = [];
       for (let i = 0; i < getUrl.formats.length; i++) {
         const item = getUrl.formats[i];
-        if (item.mimeType == 'audio/webm; codecs=\"opus\"') {
-          const {contentLength} = item;
+        if (item.mimeType == 'audio/webm; codecs="opus"') {
+          const { contentLength } = item;
           const bytes = await bytesToSize(contentLength);
-          result[i] = {audio: item.url, size: bytes};
+          result[i] = { audio: item.url, size: bytes };
         }
       }
       const resultFix = result.filter((x) => x.audio != undefined && x.size != undefined);
@@ -134,7 +134,7 @@ async function ytMp3(url) {
       const tinyUrl = tiny.data;
       const title = getUrl.videoDetails.title;
       const thumb = getUrl.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url;
-      resolve({title, result: tinyUrl, result2: resultFix, thumb});
+      resolve({ title, result: tinyUrl, result2: resultFix, thumb });
     }).catch(reject);
   });
 }
@@ -146,9 +146,9 @@ async function ytMp4(url) {
       for (let i = 0; i < getUrl.formats.length; i++) {
         const item = getUrl.formats[i];
         if (item.container == 'mp4' && item.hasVideo == true && item.hasAudio == true) {
-          const {qualityLabel, contentLength} = item;
+          const { qualityLabel, contentLength } = item;
           const bytes = await bytesToSize(contentLength);
-          result[i] = {video: item.url, quality: qualityLabel, size: bytes};
+          result[i] = { video: item.url, quality: qualityLabel, size: bytes };
         }
       }
       const resultFix = result.filter((x) => x.video != undefined && x.size != undefined && x.quality != undefined);
@@ -156,7 +156,7 @@ async function ytMp4(url) {
       const tinyUrl = tiny.data;
       const title = getUrl.videoDetails.title;
       const thumb = getUrl.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url;
-      resolve({title, result: tinyUrl, rersult2: resultFix[0].video, thumb});
+      resolve({ title, result: tinyUrl, result2: resultFix[0].video, thumb });
     }).catch(reject);
   });
 }
@@ -164,7 +164,7 @@ async function ytMp4(url) {
 async function ytPlay(query) {
   return new Promise((resolve, reject) => {
     yts(query).then(async (getData) => {
-      const result = getData.videos.slice( 0, 5 );
+      const result = getData.videos.slice(0, 5);
       const url = [];
       for (let i = 0; i < result.length; i++) {
         url.push(result[i].url);
@@ -179,7 +179,7 @@ async function ytPlay(query) {
 async function ytPlayVid(query) {
   return new Promise((resolve, reject) => {
     yts(query).then(async (getData) => {
-      const result = getData.videos.slice( 0, 5 );
+      const result = getData.videos.slice(0, 5);
       const url = [];
       for (let i = 0; i < result.length; i++) {
         url.push(result[i].url);
