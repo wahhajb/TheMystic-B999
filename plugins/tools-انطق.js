9dd2d910419e9b79a5f9e7cff9b72cf4e3f57290
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-05-16 17:15:29 +0300

    Update tools-انطق.js

--- plugins/tools-انطق.js
+++ plugins/tools-انطق.js
@@ -1,19 +1,19 @@
- import fetch from 'node-fetch';
+import fetch from 'node-fetch';
 
 const handler = async (m, { conn, usedPrefix, command, text }) => {
   const match = text.match(/^(\w+)\s*\|\s*(.+)/i);
   if (!match) {
     const voices = await getVoices();
-    const voiceNames = voices.voices.map(voice => voice.name).join('\n◉ ');
-    return m.reply(`*[❗] صيغة استخدام غير صحيحة، ينقص الصوت أو النص.*\n\n*—◉ مثال:*\n◉ ${usedPrefix + command} اسم_الصوت | النص\n\n*—◉ مثال للاستخدام:*\n◉ ${usedPrefix + command} ${voices.voices[0].name} | هذا نص تجريبي\n\n*—◉ قائمة الأصوات المتاحة:*\n◉ ${voiceNames}`
+    const voiceNames = voices.voices.map((voice, index) => `${index + 1}`).join('\n');
+    return m.reply(`*[❗] صيغة استخدام غير صحيحة، ينقص الصوت أو النص.*\n\n*—◉ مثال:*\n◉ ${usedPrefix + command} رقم_الصوت | النص\n\n*—◉ مثال للاستخدام:*\n◉ ${usedPrefix + command} 1 | هذا نص تجريبي\n\n*—◉ قائمة الأصوات المتاحة:*\n${voiceNames}`
     );
   }
-  const [, voiceName, inputText] = match;
+  const [, voiceIndex, inputText] = match;
   const voices = await getVoices();
-  const voice = voices.voices.find(voice => voice.name.toLowerCase() === voiceName.toLowerCase());
+  const voice = voices.voices[parseInt(voiceIndex) - 1];
   if (!voice) {
-    const voiceNames = voices.voices.map(voice => voice.name).join('\n◉ ');
-    return m.reply(`[❗] لم يتم العثور على أي صوت بالاسم "${voiceName}".\n\n—◉ قائمة الأصوات المتاحة:\n◉ ${voiceNames}`);
+    const voiceNames = voices.voices.map((voice, index) => `${index + 1}`).join('\n');
+    return m.reply(`[❗] لم يتم العثور على أي صوت بالرقم "${voiceIndex}".\n\n—◉ قائمة الأصوات المتاحة:\n${voiceNames}`);
   }
   const audio = await convertTextToSpeech(inputText, voice.voice_id);
   if (audio) {
