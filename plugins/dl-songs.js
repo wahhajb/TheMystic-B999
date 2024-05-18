2b0fd0894a91175ac7105ca6fc26c48b9a5b934c
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-05-18 03:41:59 +0300

    Update dl-songs.js

--- plugins/dl-songs.js
+++ plugins/dl-songs.js
@@ -1,11 +1,19 @@
- import fetch from 'node-fetch';
+import fetch from 'node-fetch';
 import yts from 'yt-search';
 import ytdl from 'ytdl-core';
 import axios from 'axios';
 import {youtubedl, youtubedlv2} from '@bochilteam/scraper';
 
+
+
 const handler = async (m, {conn, command, args, text, usedPrefix}) => {
-  if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙵𝙰𝙻𝚃𝙰𝙽𝚃𝙴, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴/𝚃𝙸𝚃𝚄𝙻𝙾 𝙳𝙴 𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} Good Feeling - Flo Rida* `;
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.downloader_playdoc
+
+
+  if (!text) throw `${tradutor.texto1} *\n*${usedPrefix + command} Good Feeling - Flo Rida* `;
   try {
     const yt_play = await search(args.join(' '));
     let additionalText = '';
@@ -14,8 +22,18 @@ const handler = async (m, {conn, command, args, text, usedPrefix}) => {
     } else if (command === 'play4' || command == 'playdoc2') {
       additionalText = 'video 🎥';
     }
-    const message = "جاري تحميل طلبك...";
-    conn.sendMessage(m.chat, message, { quoted: m });
+    const texto1 = `${tradutor.texto2[0]}\n
+    ${tradutor.texto2[1]} ${yt_play[0].title}
+    ${tradutor.texto2[2]} ${yt_play[0].ago}
+    ${tradutor.texto2[3]} ${secondString(yt_play[0].duration.seconds)}
+    ${tradutor.texto2[4]} ${`${MilesNumber(yt_play[0].views)}`}
+    ${tradutor.texto2[5]} ${yt_play[0].author.name}
+    ${tradutor.texto2[6]} ${yt_play[0].author.url}
+    ${tradutor.texto2[7]} ${yt_play[0].videoId}
+    ${tradutor.texto2[8]} ${yt_play[0].type}
+    ${tradutor.texto2[9]} ${yt_play[0].url}\n
+    ${tradutor.texto2[10]} ${additionalText}, ${tradutor.texto2[11]}`.trim();
+    conn.sendMessage(m.chat, {image: {url: yt_play[0].thumbnail}, caption: texto1}, {quoted: m});
     if (command == 'play3' || command == 'playdoc') {
       try {
         const q = '128kbps';
@@ -24,13 +42,13 @@ const handler = async (m, {conn, command, args, text, usedPrefix}) => {
         const dl_url = await yt.audio[q].download();
         const ttl = await yt.title;
         const size = await yt.audio[q].fileSizeH;
-        await conn.sendMessage(m.chat, {audio: {url: dl_url}, mimetype: 'audio/mpeg', fileName: `${ttl}.mp3`}, {quoted: m});
+        await conn.sendMessage(m.chat, {document: {url: dl_url}, mimetype: 'audio/mpeg', fileName: `${ttl}.mp3`}, {quoted: m});
       } catch {
         try {
           const lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytaudio2?apikey=${lolkeysapi}&url=${yt_play[0].url}`);
           const lolh = await lolhuman.json();
           const n = lolh.result.title || 'error';
-          await conn.sendMessage(m.chat, {audio: {url: lolh.result.link}, fileName: `${n}.mp3`, mimetype: 'audio/mpeg'}, {quoted: m});
+          await conn.sendMessage(m.chat, {document: {url: lolh.result.link}, fileName: `${n}.mp3`, mimetype: 'audio/mpeg'}, {quoted: m});
         } catch {
           try {
             const searchh = await yts(yt_play[0].url);
@@ -39,45 +57,43 @@ const handler = async (m, {conn, command, args, text, usedPrefix}) => {
             const ress = await ytdl.chooseFormat(infoo.formats, {filter: 'audioonly'});
             conn.sendMessage(m.chat, {audio: {url: ress.url}, fileName: __res[0].title + '.mp3', mimetype: 'audio/mp4'}, {quoted: m});
           } catch {
-            await conn.reply(m.chat, '*[❗] 𝙴𝚁𝚁𝙾𝚁 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝙴𝙻 𝙰𝚄𝙳𝙸𝙾*', m);
+            await conn.reply(m.chat, tradutor.texto3, m);
           }
         }
       }
     }
-    if (command == 'play4' || command == 'playdoc2') {
-      try {
-        const qu = '360';
-        const q = qu + 'p';
-        const v = yt_play[0].url;
-        const yt = await youtubedl(v).catch(async (_) => await youtubedlv2(v));
-        const dl_url = await yt.video[q].download();
-        const ttl = await yt.title;
-        const size = await yt.video[q].fileSizeH;
-        await conn.sendMessage(m.chat, {video: {url: dl_url}, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `▢ 𝚃𝙸𝚃𝚄𝙻𝙾: ${ttl}\n▢ 𝙿𝙴𝚂𝙾 𝙳𝙴𝙻 𝚅𝙸𝙳𝙴𝙾: ${size}`, thumbnail: await fetch(yt.thumbnail)}, {quoted: m});
-      } catch {
-        try {
-          const mediaa = await ytMp4(yt_play[0].url);
-          await conn.sendMessage(m.chat, {video: {url: mediaa.result}, caption: mediaa.title, mimetype: 'video/mp4', fileName: mediaa.title + `.mp4`}, {quoted: m});
-        } catch {
-          try {
-            const lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=${lolkeysapi}&url=${yt_play[0].url}`);
-            const lolh = await lolhuman.json();
-            const n = lolh.result.title || 'error';
-            const n2 = lolh.result.link;
-            const n3 = lolh.result.size;
-            const n4 = lolh.result.thumbnail;
-            await conn.sendMessage(m.chat, {video: {url: n2}, fileName: `${n}.mp4`, mimetype: 'video/mp4', caption: `▢ 𝚃𝙸𝚃𝚄𝙻𝙾: ${n}\n▢ 𝙿𝙴𝚂𝙾 𝙳𝙴𝙻 𝚅𝙸𝙳𝙴𝙾: ${n3}`, thumbnail: await fetch(n4)}, {quoted: m});
-          } catch {
-            await conn.reply(m.chat, '*[❗] 𝙴𝚁𝚁𝙾𝚁 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾*', m);
-          }
-        }
-      }
-    }
+    if (command ==  play4  || command ==  playdoc2 ) {
+  try {
+    const qu =  360 ;
+    const q = qu +  p ;
+    const v = yt_play[0].url;
+    const yt = await youtubedl(v).catch(async (_) => await youtubedlv2(v));
+    const dl_url = await yt.video[q].download();
+    const ttl = await yt.title;
+    const size = await yt.video[q].fileSizeH;
+    await await conn.sendMessage(m.chat, {video: {url: dl_url}, caption: `${tradutor.texto4[0]} ${ttl}\n${tradutor.texto4[1]} ${size}`, thumbnail: await fetch(yt.thumbnail)}, {quoted: m});
   } catch {
-    throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*';
+    try {
+      const mediaa = await ytMp4(yt_play[0].url);
+      await await conn.sendMessage(m.chat, {video: {url: dl_url}, caption: cap}, {quoted: m});
+    } catch {
+      try {
+        const lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=${lolkeysapi}&url=${yt_play[0].url}`);
+        const lolh = await lolhuman.json();
+        const n = lolh.result.title ||  error ;
+        const n2 = lolh.result.link;
+        const n3 = lolh.result.size;
+        const n4 = lolh.result.thumbnail;
+        await conn.sendMessage(m.chat, {video: {url: n2}, caption: `${tradutor.texto4[0]} ${n}\n${tradutor.texto4[1]} ${n3}`, thumbnail: await fetch(n4)}, {quoted: m});
+      } catch {
+        await conn.reply(m.chat, `${tradutor.texto5}`, m);
+      }
+    }
+  }
+}catch {
+    throw `${tradutor.texto6}}`;
   }
 };
-
 handler.help = ['play3', 'play4'].map((v) => v + ' < busqueda >');
 handler.tags = ['downloader'];
 handler.command = /^(playdoc|playdoc2|play3|play4)$/i;
@@ -97,9 +113,6 @@ function MilesNumber(number) {
 }
 
 function secondString(seconds) {
-  seconds = Number(seconds);
-  const d = Math.floor(seconds / (3600 * 24));
-  const h = Mathfunction secondString(seconds) {
   seconds = Number(seconds);
   const d = Math.floor(seconds / (3600 * 24));
   const h = Math.floor((seconds % (3600 * 24)) / 3600);
@@ -128,7 +141,7 @@ async function ytMp3(url) {
       const result = [];
       for (let i = 0; i < getUrl.formats.length; i++) {
         const item = getUrl.formats[i];
-        if (item.mimeType == 'audio/webm; codecs="opus"') {
+        if (item.mimeType == 'audio/webm; codecs=\"opus\"') {
           const {contentLength} = item;
           const bytes = await bytesToSize(contentLength);
           result[i] = {audio: item.url, size: bytes};
@@ -161,7 +174,7 @@ async function ytMp4(url) {
       const tinyUrl = tiny.data;
       const title = getUrl.videoDetails.title;
       const thumb = getUrl.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url;
-      resolve({title, result: tinyUrl, result2: resultFix[0].video, thumb});
+      resolve({title, result: tinyUrl, rersult2: resultFix[0].video, thumb});
     }).catch(reject);
   });
 }
@@ -169,7 +182,7 @@ async function ytMp4(url) {
 async function ytPlay(query) {
   return new Promise((resolve, reject) => {
     yts(query).then(async (getData) => {
-      const result = getData.videos.slice(0, 5);
+      const result = getData.videos.slice( 0, 5 );
       const url = [];
       for (let i = 0; i < result.length; i++) {
         url.push(result[i].url);
@@ -184,7 +197,7 @@ async function ytPlay(query) {
 async function ytPlayVid(query) {
   return new Promise((resolve, reject) => {
     yts(query).then(async (getData) => {
-      const result = getData.videos.slice(0, 5);
+      const result = getData.videos.slice( 0, 5 );
       const url = [];
       for (let i = 0; i < result.length; i++) {
         url.push(result[i].url);
@@ -195,3 +208,47 @@ async function ytPlayVid(query) {
     }).catch(reject);
   });
 }
+
+
+// import { youtubeSearch } from '@bochilteam/scraper'
+// import fetch from 'node-fetch'
+// let handler = async (m, { conn, command, text, usedPrefix }) => {
+// if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙵𝙰𝙻𝚃𝙰𝙽𝚃𝙴, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴/𝚃𝙸𝚃𝚄𝙻𝙾 𝙳𝙴 𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} Good Feeling - Flo Rida*`
+// try {
+// let vid = (await youtubeSearch(text)).video[0]
+// let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
+// const urll = 'https://www.youtube.com/watch?v=' + videoId
+// var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
+// var document = doc[Math.floor(Math.random() * doc.length)]
+// const buttons = [
+// { buttonId: `#ytmp3doc ${urll}`, buttonText: { displayText: '🎵 𝐀𝐔𝐃𝐈𝐎𝐃𝐎𝐂 🎵' }, type: 1 },
+// { buttonId: `#ytmp4doc ${urll}`, buttonText: { displayText: '🎥 𝐕𝐈𝐃𝐄𝐎𝐃𝐎𝐂 🎥' }, type: 1 },
+// { buttonId: `#playlist ${text}`, buttonText: { displayText: '📋 𝐌𝐀𝐒 𝐑𝐄𝐒𝐔𝐋𝐓𝐀𝐃𝐎𝐒 📋' }, type: 1 }, ]
+// let texto1 = `*◉—⌈🔊 𝐏𝐋𝐀𝐘 𝐃𝐎𝐂𝐔𝐌𝐄𝐍𝐓 🔊⌋—◉*\n
+// ❏ 📌 *𝚃𝙸𝚃𝚄𝙻𝙾:* ${title}
+// ❏ 📆 *𝙿𝚄𝙱𝙻𝙸𝙲𝙰𝙳𝙾:* ${publishedTime}
+// ❏ ⌚ *𝙳𝚄𝚁𝙰𝙲𝙸𝙾𝙽:* ${durationH}
+// ❏ 👀 *𝚅𝙸𝚂𝚃𝙰𝚂:* ${viewH}
+// ❏ 📇 *𝙳𝙴𝚂𝙲𝚁𝙸𝙿𝙲𝙸𝙾𝙽:* ${description}
+// ❏ 🔗 *𝙻𝙸𝙽𝙺:* ${urll}`.trim()
+// let buttonMessage = { "document": { url: "https://wa.me/5219992095479" }, "fileName": '❏ 🌿 ʀᴇᴘʀᴏᴅᴜᴄᴛᴏʀ ᴅᴇ ʏᴏᴜᴛᴜʙᴇ', "mimetype": 'application/vnd.ms-excel', "caption": texto1, "fileLength": '99999999999999', "mentions": [m.sender], "footer": wm, "buttons": buttons, "headerType": 4, contextInfo: { "mentionedJid": [m.sender], "externalAdReply": { "showAdAttribution": true, "title": `${title}`, "mediaType": 2, "previewType": "VIDEO", "thumbnail": await (await fetch(thumbnail)).buffer(), "mediaUrl": `${urll}`, "sourceUrl": `https://github.com/BrunoSobrino/TheMystic-Bot-MD` }}}
+// conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+// } catch {
+// try {
+// let vid2 = await (await fetch(`https://api.lolhuman.xyz/api/ytsearch?apikey=${lolkeysapi}&query=${text}`)).json()
+// let { videoId, title, views, published, thumbnail } = await vid2.result[0]
+// const url = 'https://www.youtube.com/watch?v=' + videoId
+// let ytLink = await fetch(`https://api.lolhuman.xyz/api/ytplay2?apikey=${lolkeysapi}&query=${text}`)
+// let jsonn = await ytLink.json()
+// let aud = await jsonn.result.audio
+// let capt = `❏ 📌 *𝚃𝙸𝚃𝚄𝙻𝙾:* ${title}\n❏ 📆 *𝙿𝚄𝙱𝙻𝙸𝙲𝙰𝙳𝙾:* ${published}\n❏ 👀 *𝚅𝙸𝚂𝚃𝙰𝚂:* ${views}\n❏ 🔗 *𝙻𝙸𝙽𝙺:* ${url}`
+// const buttons = [{buttonId: `#playlist ${title}`, buttonText: {displayText: '📋 𝐌𝐀𝐒 𝐑𝐄𝐒𝐔𝐋𝐓𝐀𝐃𝐎𝐒 📋'}, type: 1}]
+// const buttonMessage = { image: {url: thumbnail}, caption: capt, footer: '*ᴇɴᴠɪᴀɴᴅᴏ ᴀᴜᴅɪᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*', buttons: buttons, headerType: 4 }
+// let msg = await conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+// conn.sendMessage(m.chat, { document: { url: aud }, mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, {quoted: msg})
+// } catch {
+// throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*'}}}
+// handler.help = ['playdoc', 'play3'].map(v => v + ' <pencarian>')
+// handler.tags = ['downloader']
+// handler.command = /^play3|playdoc?$/i
+// export default handler
