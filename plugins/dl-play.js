199c97288b864359340511f4113380c8de013771
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-05-18 19:36:15 +0300

    Add files via upload

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,4 +1,5 @@
- import ytdl from 'ytdl-core';
+
+import ytdl from 'ytdl-core';
 import yts from 'yt-search';
 import fs from 'fs';
 import { pipeline } from 'stream';
@@ -7,84 +8,75 @@ import os from 'os';
 
 const streamPipeline = promisify(pipeline);
 
-var loading = async (conn, m, response) => {
-    var hawemod = [
-        "《 █▒▒▒▒▒▒▒▒▒▒▒》10%",
-        "《 ████▒▒▒▒▒▒▒▒》30%",
-        "《 ███████▒▒▒▒▒》50%",
-        "《 ██████████▒▒》80%",
-        "《 ████████████》100%"
-    ];
-    let { key } = await conn.sendMessage(m.chat, { text: ">جاري تحميل طلبك...", mentions: conn.parseMention(response) }, { quoted: m });
-    for (let i = 0; i < hawemod.length; i++) {
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        await conn.sendMessage(m.chat, { text: hawemod[i], edit: key, mentions: conn.parseMention(response) }, { quoted: m });
-    }
-    await conn.sendMessage(m.chat, { text: response, edit: key, mentions: conn.parseMention(response) }, { quoted: m });
-};
-
 var handler = async (m, { conn, command, text, usedPrefix }) => {
-    if (!text) throw `*${usedPrefix}${command} اية الكرسي*`;
+  if (!text) throw `*${usedPrefix}${command} اية الكرسي*`;
 
-    await loading(conn, m, "Downloading audio please wait");
+  let search = await yts(text);
+  let vid = search.videos[Math.floor(Math.random() * search.videos.length)];
+  if (!search) throw 'Video Not Found, Try Another Title';
+  let { title, thumbnail, timestamp, views, ago, url } = vid;
+  let wm = 'Downloading audio please wait';
 
-    let search = await yts(text);
-    let vid = search.videos[Math.floor(Math.random() * search.videos.length)];
-    if (!search) throw "Video Not Found, Try Another Title";
-    let { title, thumbnail, timestamp, views, ago, url } = vid;
-    let captvid = `*❖───┊ ♪ يــوتـــيــوب ♪ ┊───❖*
-    ❏ الـعـنوان: ${title}
-    
-    ❐ الـمده: ${timestamp}
-    
-    ❑ الــمـشهـدات: ${views}
-    
-    ❒ مـنذ: ${ago}
-    
-    ❒ الـرابــط: ${url}`;
+  let captvid = `*❖───┊ ♪ يــوتـــيــوب ♪ ┊───❖*
+  ❏ الـعـنوان: ${title}
+  
+  ❐ الـمده: ${timestamp}
+  
+  ❑ الــمـشهـدات: ${views}
+  
+  ❒ مـنذ: ${ago}
+  
+  ❒ الـرابــط: ${url}`;
 
-    conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author }, { quoted: m });
+  conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author }, { quoted: m });
 
-    const audioStream = ytdl(url, {
-        filter: 'audioonly',
-        quality: 'highestaudio',
-    });
 
-    const tmpDir = os.tmpdir();
-    const writableStream = fs.createWriteStream(`${tmpDir}/${title}.mp3`);
-    await streamPipeline(audioStream, writableStream);
+  const audioStream = ytdl(url, {
+    filter: 'audioonly',
+    quality: 'highestaudio',
+  });
 
-    let doc = {
-        audio: {
-            url: `${tmpDir}/${title}.mp3`
-        },
-        mimetype: 'audio/mp4',
-        fileName: `${title}`,
-        contextInfo: {
-            externalAdReply: {
-                showAdAttribution: true,
-                mediaType: 2,
-                mediaUrl: url,
-                title: title,
-                body: "Downloading audio please wait",
-                sourceUrl: url,
-                thumbnail: await (await conn.getFile(thumbnail)).data
-            }
-        }
-    };
+  // Get the path to the system's temporary directory
+  const tmpDir = os.tmpdir();
 
-    await conn.sendMessage(m.chat, doc, { quoted: m });
+  // Create writable stream in the temporary directory
+  const writableStream = fs.createWriteStream(`${tmpDir}/${title}.mp3`);
 
-    fs.unlink(`${tmpDir}/${title}.mp3`, (err) => {
-        if (err) {
-            console.error(`Failed to delete audio file: ${err}`);
-        } else {
-            console.log(`Deleted audio file: ${tmpDir}/${title}.mp3`);
-        }
-    });
+  // Start the download
+  await streamPipeline(audioStream, writableStream);
+
+  let doc = {
+    audio: {
+      url: `${tmpDir}/${title}.mp3`
+    },
+    mimetype: 'audio/mp4',
+    fileName: `${title}`,
+    contextInfo: {
+      externalAdReply: {
+        showAdAttribution: true,
+        mediaType: 2,
+        mediaUrl: url,
+        title: title,
+        body: wm,
+        sourceUrl: url,
+        thumbnail: await (await conn.getFile(thumbnail)).data
+      }
+    }
+  };
+
+  await conn.sendMessage(m.chat, doc, { quoted: m });
+
+  // Delete the audio file
+  fs.unlink(`${tmpDir}/${title}.mp3`, (err) => {
+    if (err) {
+      console.error(`Failed to delete audio file: ${err}`);
+    } else {
+      console.log(`Deleted audio file: ${tmpDir}/${title}.mp3`);
+    }
+  });
 };
 
-handler.help = ['play <query>'];
+handler.help = ['play'].map((v) => v + ' <query>');
 handler.tags = ['downloader'];
 handler.command = /^شغل$/i;
 
