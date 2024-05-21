9f88d464a3ada215c417b91dfdfe02f0fe5392a5
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-05-21 21:31:51 +0300

    Update dl-صوره.js

--- plugins/dl-صوره.js
+++ plugins/dl-صوره.js
@@ -1,37 +1,66 @@
-import { googleImage } from  @bochilteam/scraper ;
+/**
+ * @type {import('@whiskeysockets/baileys')}
+ */
+const {
+  proto,
+  generateWAMessage,
+  areJidsSameUser,
+  decryptPollVote,
+} = (await import('@whiskeysockets/baileys')).default;
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `*[❗خطاء❗] مثال على الأمر ${usedPrefix + command} أحمد طرزان*`;
-    if (m.text.match(/\b(gore|cp|porno|xxx|sex|سكس|طيز|خرق|نيك|قحبه|قحبة|porn|xxnx|xnxx|خرقك)\b/i)) 
-        return m.reply( [❗خطاء❗] لا يمكنني إرسال هذا المحتوى، المجموعة محظورة\nإذا كنت مشرفًا وتريد تنشيطها، أخبر المطور );
-    
+import { googleImage } from '@bochilteam/scraper';
+
+export async function all(m, chatUpdate) {
+  if (m.isBaileys) {
+    return;
+  }
+  if (!m.message) {
+    return;
+  }
+  if (!(m.message.buttonsResponseMessage || m.message.templateButtonReplyMessage || m.message.listResponseMessage || m.message.interactiveResponseMessage)) {
+    return;
+  }
+  let id;
+  if (m.message.buttonsResponseMessage) {
+    id = m.message.buttonsResponseMessage.selectedButtonId;
+  } else if (m.message.templateButtonReplyMessage) {
+    id = m.message.templateButtonReplyMessage.selectedId;
+  } else if (m.message.listResponseMessage) {
+    id = m.message.listResponseMessage.singleSelectReply?.selectedRowId;
+  } else if (m.message.interactiveResponseMessage) {
+    id = JSON.parse(m.message.interactiveResponseMessage.nativeFlowResponseMessage.paramsJson).id;
+  }
+
+  const [command, url] = id.split(' ');
+
+  if (command === 'sendimage' && url) {
     try {
-        const res = await googleImage(text);
-        const images = res.slice(0, 5);  // اختر 5 صور للعرض عبر الأزرار
-
-        const buttons = images.map((image, index) => ({
-            buttonId: `sendimage ${image.url}`,
-            buttonText: { displayText: `صورة ${index + 1}` },
-            type: 1
-        }));
-
-        const buttonMessage = {
-            text: `🔎 *النتائج التي تم الحصول عليها ل:* ${text}\n🌎 *محرك البحث:* جوجل`,
-            footer:  اختر الصورة التي تريد إرسالها ,
-            buttons: buttons,
-            headerType: 1
-        };
-
-        await conn.sendMessage(m.chat, buttonMessage, { quoted: m });
-        console.log(`تم إرسال الأزرار للمستخدم مع نتائج البحث عن: ${text}`);
+      await conn.sendFile(m.chat, url, 'image.jpg', `🔗 *من:* ${url}`, m);
+      console.log(`تم إرسال الصورة: ${url}`);
     } catch (e) {
-        console.error(e);
-        m.reply( حدث خطأ أثناء البحث عن الصور. );
+      console.error(e);
+      await conn.sendMessage(m.chat, 'حدث خطأ أثناء إرسال الصورة.', m);
     }
+  } else if (command === '.صورة' && text) {
+    if (m.text.match(/\b(gore|cp|porno|xxx|sex|سكس|طيز|خرق|نيك|قحبه|قحبة|porn|xxnx|xnxx|خرقك)\b/i)) 
+      return m.reply('[❗خطاء❗] لا يمكنني إرسال هذا المحتوى، المجموعة محظورة\nإذا كنت مشرفًا وتريد تنشيطها، أخبر المطور');
+
+    const res = await googleImage(text);
+    const images = res.slice(0, 5);  // اختر 5 صور للعرض عبر الأزرار
+
+    const buttons = images.map((image, index) => ({
+        buttonId: `sendimage ${image.url}`,
+        buttonText: { displayText: `صورة ${index + 1}` },
+        type: 1
+    }));
+
+    const buttonMessage = {
+        text: `🔎 *النتائج التي تم الحصول عليها ل:* ${text}\n🌎 *محرك البحث:* جوجل`,
+        footer: 'اختر الصورة التي تريد إرسالها',
+        buttons: buttons,
+        headerType: 1
+    };
+
+    await conn.sendMessage(m.chat, buttonMessage, { quoted: m });
+  }
 }
-
-handler.help = [ gimage <query> ,  imagen <query> ];
-handler.tags = [ internet ,  tools ];
-handler.command = /^(صورة|image|صوره|imagen)$/i;
-
-export default handler;
