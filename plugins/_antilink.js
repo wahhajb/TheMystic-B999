4e552394dc86d2984ed4a0e59eea73edcadb4c34
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-05-21 17:02:28 +0300

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,37 +1,34 @@
 // TheMystic-Bot-MD@BrunoSobrino - _antilink.js
 
-  
-const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
-export async function before(m, {conn, isAdmin, isBotAdmin}) {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins._antilink
+const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i;
 
-  if (m.isBaileys && m.fromMe) {
-    return !0;
-  }
-  if (!m.isGroup) return !1;
-  const chat = global.db.data.chats[m.chat];
-  const delet = m.key.participant;
-  const bang = m.key.id;
-  const bot = global.db.data.settings[this.user.jid] || {};
-  const user = `@${m.sender.split`@`[0]}`;
-  const isGroupLink = linkRegex.exec(m.text);
-  const grupo = `https://chat.whatsapp.com`;
-  if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply(tradutor.texto1);
-  if (chat.antiLink && isGroupLink && !isAdmin) {
-    if (isBotAdmin) {
-      const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
-      if (m.text.includes(linkThisGroup)) return !0;
+export async function before(m, {conn, isAdmin, isBotAdmin}) {
+    if (m.isBaileys && m.fromMe) return !0;
+    if (!m.isGroup) return !1;
+
+    let chat = global.db.data.chats[m.chat];
+    let bot = global.db.data.settings[this.user.jid] || {};
+    const isGroupLink = linkRegex.exec(m.text);
+
+    // إضافة الشرط الإداري
+    if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply("*﮼♪ﺑوﺕ|الصاعـ|ـقة⁞¹² تم اكتشاف ارسال رابط في الجروب*\n\n هناك مخالفه من احد الاعضاء جاري الكشف عن هويته...! \nانا اسف, *@${m.sender.split('@')[0]}*  سيتم طردك من الجروب بسبب مخالفتك للقوانين الخاصه بالجروب وارسال روابط البوت\n ΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩθ ${isBotAdmin ? '' : '\n اوههةة نسيت انا مو مشرف هون\n ياليت كنت مشرف هون كنت بنتناكح هون وشوتك برا الجروب☻⃤'}", null, {mentions: [m.sender]});
+
+    if (chat.antiLink && isGroupLink && !isAdmin) {
+        if (isBotAdmin) {
+            const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
+            if (m.text.includes(linkThisGroup)) return !0;
+        }
+
+        // إضافة رسالة التحذير
+        await conn.reply(m.chat, `تم اكتشاف ارسال رابط في الجروب...`, null, {mentions: [m.sender]});
+
+        // إضافة الإجراءات الإدارية
+        if (isBotAdmin && chat.antiLink) {
+            await conn.sendMessage(m.chat, {delete: m.key});
+            await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+        } else if (!chat.antiLink) {
+            return;
+        }
     }
-    await this.sendMessage(m.chat, {text: tradutor.texto2, mentions: [m.sender]}, {quoted: m});
-    if (!isBotAdmin) return m.reply(tradutor.texto3);
-    if (isBotAdmin && bot.restrict) {
-      await conn.sendMessage(m.chat, {delete: {remoteJid: m.chat, fromMe: false, id: bang, participant: delet}});
-      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-      if (responseb[0].status === '404') return;
-    } else if (!bot.restrict) return m.reply(tradutor.texto4);
-  }
-  return !0;
+    return !0;
 }
