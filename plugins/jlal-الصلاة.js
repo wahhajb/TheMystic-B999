343c4ffec0cb65c14231036bfd3d1e66e528bdd7
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-05-23 01:55:35 +0300

    Update jlal-الصلاة.js

--- plugins/jlal-الصلاة.js
+++ plugins/jlal-الصلاة.js
@@ -1,9 +1,9 @@
-
-import axios from  axios ;
+// قناتي https://whatsapp.com/channel/0029VadAOFcL2ATzSf46sI0r
+import axios from 'axios';
 
 const handler = async (m, { text }) => {
     if (!text) {
-        return m.reply( يرجى تحديد اسم المدينة لعرض مواقيت الصلاة. مثال:\n\n.الصلاة صنعاء );
+        return m.reply("يرجى تحديد اسم المدينة لعرض مواقيت الصلاة. مثال:\n\n.الصلاة القاهرة");
     }
 
     try {
@@ -13,15 +13,12 @@ const handler = async (m, { text }) => {
         const message = `مواقيت الصلاة في ${text} اليوم:\n- الفجر: ${prayerData.Fajr}\n- الشروق: ${prayerData.Sunrise}\n- الظهر: ${prayerData.Dhuhr}\n- العصر: ${prayerData.Asr}\n- المغرب: ${prayerData.Maghrib}\n- العشاء: ${prayerData.Isha}`;
         m.reply(message);
     } catch (error) {
-        console.error( حدث خطأ في الحصول على مواقيت الصلاة: , error);
-        m.reply( عذرًا، لم أتمكن من العثور على مواقيت الصلاة لهذه المدينة. );
+        console.error("حدث خطأ في الحصول على مواقيت الصلاة:", error);
+        m.reply("عذرًا، لم أتمكن من العثور على مواقيت الصلاة لهذه المدينة.");
     }
 }
 
-handler.command = [ الصلاة ,  مواقيت_الصلاة ]
-handler.tags = [ tools ]
+handler.command = ['الصلاة', 'مواقيت_الصلاة'];
+handler.tags = ['tools'];
 
-export default handler
-
-
-> ROPERTO AL MASRY
+export default handler;
