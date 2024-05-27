c0208f68542fe74c7591d1675e9af99c59d40a05
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-05-27 23:59:32 +0300

    Update gp-تغيرالاسم.js

--- plugins/gp-تغيرالاسم.js
+++ plugins/gp-تغيرالاسم.js
@@ -1,4 +1,4 @@
-import Presence from '@adiwajshing/baileys'
+import Presence from '@whiskeysockets/baileys'
 let handler  = async (m, { conn, args, text }) => {
 if (!text) throw `*مثال : .تغير-الاسم اكتب اسم الجروب الجديد*`
 try {
