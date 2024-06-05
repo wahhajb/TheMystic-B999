4c197e4c8cc11dfd2214c221b546a1b12aba1650
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-05 18:59:43 +0300

    Update _مواعيد-الاذان.js

--- plugins/_مواعيد-الاذان.js
+++ plugins/_مواعيد-الاذان.js
@@ -22,7 +22,7 @@ export async function before(m) {
     const timeNow = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
     for (const [sholat, waktu] of Object.entries(jadwalSholat)) {
         if (timeNow === waktu) {
-            let caption = `السلام  عليكم   *${name}*,\nحان موعد أدان صلاة *${sholat}* اذهب و توضأ بسرعة و قم لصلاتك  ولا تنسانا من الدعاء.\n\n*${waktu}*\n> *هذا توقيت الصلاة في صنعاء وما جاورها*`;
+            let caption = `السلام  عليكم   *${name}*,\nحان موعد أذان صلاة *${sholat}* اذهب و توضأ بسرعة و قم لصلاتك  ولا تنسانا من الدعاء.\n\n*${waktu}*\n> *هذا توقيت الصلاة في صنعاء وضواحيها*`;
             this.autosholat[id] = [
                 this.reply(m.chat, caption, null),
                 setTimeout(() => {
