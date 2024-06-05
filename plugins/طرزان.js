117f60f4e9091b0246b332b05c24bafc1872c370
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-06 02:03:41 +0300

    Update طرزان.js

--- plugins/طرزان.js
+++ plugins/طرزان.js
@@ -5,39 +5,39 @@ const handler = async (m, { conn, command, text }) => {
   const getRandomMessage = (messages) => messages[Math.floor(Math.random() * messages.length)];
   const response =
     `✦•━━━━ ∘⊰🔥⊱∘ ━━━━•✦\n` +
-    `${text} خلاصة: الاسطورة احمد طرزان هو فحل المجال لهذا العام🦅🇾🇪  ` +
+    `${text}😂♥️ وينك عيوشه لساتك زعلانه ` +
     `\n✦•━━━━ ∘⊰🔥⊱∘ ━━━━•✦` +
     ``    
 
   async function loading() {
-    var hawemod = [
-      "عمك",
-      "الاسطورة",
-      "احمد طرزان",
-      "هو فحل العالم",
-      "ونايك",
-      "كسم المجال",
-      "وفحل",
-      "كمن",
-      "شرموط",
-      "يعمل",
-      "في مطاردة",
-      "الخرفان",
-      "ونيك ارقامهم",
-      "وتشرديهم",
-      "من كل",
-      "جروب🤤🤟🏿"
-    ];
-    let { key } = await conn.sendMessage(m.chat, {text: `⌯ نبذة تعريفيه عن فحل المجال`, mentions: conn.parseMention(response)}, {quoted: m})
-    for (let i = 0; i < hawemod.length; i++) {
-      await new Promise(resolve => setTimeout(resolve, 1000)); 
-      await conn.sendMessage(m.chat, {text: hawemod[i], edit: key, mentions: conn.parseMention(response)}, {quoted: m}); 
-    }
-    await conn.sendMessage(m.chat, {text: response, edit: key, mentions: conn.parseMention(response)}, {quoted: m});         
+var hawemod = [
+      "⌯ عمك",
+      "⌯ الاسطورة",
+      "⌯ احمد طرزان",
+      "⌯ هو فحل العالم",
+      "⌯ ونايك",
+      "⌯ كسم المجال",
+      "⌯ وفحل",
+      "⌯ كمن",
+      "⌯ شرموط",
+      "⌯ يعمل",
+      "⌯ في مطاردة",
+      "⌯ الخرفان",
+      "⌯ ونيك ارقامهم",
+      "⌯ وتشرديهم",
+      "⌯ من كل",
+      "⌯ جروب🤤🤟🏿"
+]
+   let { key } = await conn.sendMessage(m.chat, {text: `⌯ زعلانه عيوشه`, mentions: conn.parseMention(response)}, {quoted: m})
+ for (let i = 0; i < hawemod.length; i++) {
+   await new Promise(resolve => setTimeout(resolve, 1000)); 
+   await conn.sendMessage(m.chat, {text: hawemod[i], edit: key, mentions: conn.parseMention(response)}, {quoted: m}); 
   }
-  loading()    
+  await conn.sendMessage(m.chat, {text: response, edit: key, mentions: conn.parseMention(response)}, {quoted: m});         
+ }
+loading()    
 };
-handler.help = [ love ];
-handler.tags = [ fun ];
-handler.command = /^(طرزان|احمدطرزان)$/i;
+handler.help = ['love'];
+handler.tags = ['fun'];
+handler.command = /^(طرزان)$/i;
 export default handler;
