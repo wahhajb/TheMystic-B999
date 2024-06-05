19088b10b380f1855cf479bc099d0d1b930263d7
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-05 19:08:20 +0300

    Update _مواعيد-الاذان.js

--- plugins/_مواعيد-الاذان.js
+++ plugins/_مواعيد-الاذان.js
@@ -1,12 +1,15 @@
 export async function before(m) {
-    this.autosholat = this.autosholat ? this.autosholat : {};
-    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender;
-    let name = await this.getName(who);
-    let id = m.chat;
+    this.autosholat = this.autosholat || {};
+    const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender;
+    const name = await this.getName(who);
+    const id = m.chat;
+
     if (id in this.autosholat) {
+        console.log(`Reminder already set for chat ID: ${id}`);
         return false;
     }
-    let jadwalSholat = {
+
+    const jadwalSholat = {
         الفجر: "04:15",
         الضحى: "05:50",
         الظهر: "12:00",
@@ -14,15 +17,19 @@ export async function before(m) {
         المغرب: "18:30",
         العشاء: "19:45"
     };
-    const date = new Date(new Date().toLocaleString("en-US", {
-      timeZone: "Asia/Aden"
-    }));
+
+    const date = new Date(new Date().toLocaleString("en-US", { timeZone: "Asia/Aden" }));
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const timeNow = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+
+    console.log(`Current time: ${timeNow}`);
+
     for (const [sholat, waktu] of Object.entries(jadwalSholat)) {
+        console.log(`Checking time for ${sholat} at ${waktu}`);
         if (timeNow === waktu) {
-            let caption = `السلام  عليكم   *${name}*,\nحان موعد أذان صلاة *${sholat}* اذهب و توضأ بسرعة و قم لصلاتك  ولا تنسانا من الدعاء.\n\n*${waktu}*\n> *هذا توقيت الصلاة في صنعاء وضواحيها*`;
+            console.log(`Matched time for ${sholat}`);
+            const caption = `السلام عليكم *${name}*,\nحان موعد أدان صلاة *${sholat}* اذهب وتوضأ بسرعة وقم لصلاتك ولا تنسانا من الدعاء.\n\n*${waktu}*\n> *هذا توقيت الصلاة في صنعاء وما جاورها*`;
             this.autosholat[id] = [
                 this.reply(m.chat, caption, null),
                 setTimeout(() => {
