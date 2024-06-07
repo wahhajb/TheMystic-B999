8c751811a186c3b49fb0b8055c0d13073c6802f9
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-06-08 01:19:15 +0300

    Update fun-تويت.js

--- plugins/fun-تويت.js
+++ plugins/fun-تويت.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text }) => {
-    if (!text) throw 'No Text';
+    if (!text) throw 'اكتب اي شي!';
   
     const avatar = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png');
     const displayName = conn.getName(m.sender);
